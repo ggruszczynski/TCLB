81679f28ac7c6c04b22ae3e117deb6c8b0e1dbdf
muaddieb <ggruszczynski@gmail.com>
2018-05-16 18:35:08 +1000

    some cleanup

--- moje/python/u_spurious_plotter.py
+++ moje/python/u_spurious_plotter.py
@@ -5,9 +5,7 @@ import os
 from utilites import remove_duplicates_y
 
 
-
-rho_ratio = [1, 10, 100, 1000]
-u_MRT_Guo = [6.00E-08, 1.10E-07, 1.30E-07, 1.30E-07]
+rho_ratio = '100'
 
 def read_data(filename):
     x = np.empty(0)
@@ -25,21 +23,24 @@ def read_data(filename):
     return x, u
 
 
-x, u_lin10 = read_data("u_spurious_MRT_lin_rho10.csv")
+x23, u_eq23_rho10 = read_data("u_spurious_MRT_eq23_rho%s.csv" % rho_ratio)
+x25, u_eq25_rho10 = read_data("u_spurious_MRT_eq25_rho%s.csv" % rho_ratio)
+
 
 # make plot
+plt.rcParams.update({'font.size': 24})
 plt.figure(figsize=(12, 8))
 # plt.plot(rho_ratio, u_MRT_Guo, color="red", marker=".", linestyle="", label='u MRT linear relaxation')
-plt.plot(x/x.max(), u_lin10, color="green", label=r'$u MRT linear relaxation \rho^* = 10$')
+plt.semilogy(x23 / x23.max(), u_eq23_rho10.flatten(), color="green", label=r'$ eq23$')
+plt.semilogy(x25 / x25.max(), u_eq25_rho10.flatten(), color="red", label=r'$ eq25$')
 
-# plt.plot(x, u1/u_avg, color="blue", marker="", linestyle="--", label="through the centre of air water interface")
 plt.xlabel(r'$x$')
 plt.ylabel(r'$u$')
 
 plt.title('Spurious currents')
 plt.grid(True)
 plt.legend()
-
+plt.text(0.0, 5E-6, r'$\rho^* = %s$' % rho_ratio)
 fig = plt.gcf()  # get current figure
-fig.savefig('spurious_currents.png')
+fig.savefig('spurious_currents_rho%s.png' % rho_ratio)
 plt.show()
