b1db1592082d59ca8073e50ed3b91af83fb09f77
grzegorz <ggruszczynski@gmail.com>
2024-04-23 12:27:22 +0200

    chi porosity

--- example/porous_media/generate_media.py
+++ example/porous_media/generate_media.py
@@ -11,13 +11,13 @@ import cv2
 # %% Attempt 2
 
 # defaults
-# shape = (1024,1024)
+# shape = (256,1024)
 # scale = 100.0
 # octaves = 6
 # persistence = 0.5
 # lacunarity = 2.0
 # defaults
-shape = (256,1024) # (heigh, width)
+shape = (1024,1536) # (heigh, width)
 
 
 # Generate a random base each time
@@ -41,29 +41,30 @@ def make_world(shape, randbaseint=0):
             
     return world
 
-world = make_world(shape, randbaseint=0)
-world2 = make_world(shape, randbaseint=0)
+world1 = make_world(shape, randbaseint=0)
+world2 = make_world(shape, randbaseint=1)
 
-make_plot(world,title='Porosity', cmap='gray')
-make_histogram(world, title="Porosity")  
-save_img(world, h=shape[0], w=shape[1], cmap='gist_gray', title='generated_porosity.png')
+make_plot(world1,title='Porosity - normal distribution', cmap='gray')
+make_histogram(world1, title="Porosity - normal distribution ")  
+
+velocity = np.sqrt(world1**2 + world2 **2)
+make_plot(velocity,title='Porosity - Boltzmann (Chi) distribution', cmap='gray')
+make_histogram(velocity, title="Porosity - Boltzmann (Chi) distribution") 
+
+save_img(velocity, h=shape[0], w=shape[1], cmap='gist_gray', title='generated_porosity_chi_distribution.png')
+
 
 # Adjusted Kozeny-Carman equation to include epsilon in the denominator
 def Kozeny_Carman(phi,K=1E0,S=1E-0, epsilon = 1E-2):
     return (phi**3) / (K * ((1 - phi)**2 + epsilon)) * (1/S**2)
 
-
-velocity = np.sqrt(world**2 + world2 **2)
-make_histogram(velocity, title="np.sqrt(world**2 + world2 **2)") 
-
 kc_world = Kozeny_Carman(velocity)
+make_histogram(kc_world, title="Raw KC permability from Chi distributed porosity")
+make_plot(kc_world, title='Raw KC permability from Chi distributed porosity', cmap='gray')
 
-make_histogram(kc_world, title="Raw KC permability")
-make_plot(kc_world,title='Raw KC permability', cmap='gray')
 
 def tanh_limiter(signal, threshold):
     return np.tanh(signal / threshold)
-
 
 kc_world_compressed = tanh_limiter(kc_world, 0.2)
 # kc_world_compressed= np.log(np.abs(kc_world)+1E-2)
